c02d776da5f2ca4365ef5e3ee9ed214ae3e51807
Valentin Kennke <valentin.kennke@t-systems.com>
2020-08-14 21:22:27 +0200

    closes #1
    (cherry picked from commit 3446ef3282241f7dea430d367c66876358eb874e)

--- projects/ng-http-date-core/src/lib/ng-http-date.interceptor.spec.ts
+++ projects/ng-http-date-core/src/lib/ng-http-date.interceptor.spec.ts
@@ -2,6 +2,7 @@
 import {NgHttpDateInterceptor} from './ng-http-date.interceptor';
 import {NgHttpDateConfiguration} from './ng-http-date.configuration';
 import {NgHttpDateDateConverter} from './ng-http-date.date-converter';
+import {HttpResponse} from '@angular/common/http';
 
 describe('NgHttpDateInterceptor', () => {
   let interceptor: NgHttpDateInterceptor;
@@ -10,72 +11,76 @@ describe('NgHttpDateInterceptor', () => {
     interceptor = new NgHttpDateInterceptor(new NgHttpDateConfiguration(), [new NgHttpDateDateConverter()]);
   });
 
-  it('#convertToDate should convert (with default config) YYYY strings to Date', () => {
-    expect(interceptor['convertToDate']('1988').toISOString()).toBe('1988-01-01T00:00:00.000Z');
+  it('#testAndConvert should convert (with default config) YYYY strings to Date', () => {
+    test(interceptor, {testDate: '1988'}, {testDate: new Date('1988')});
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04').toISOString()).toBe('1988-04-01T00:00:00.000Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM strings to Date', () => {
+    test(interceptor, {testDate: '1988-04', other: 12345}, {
+      testDate: new Date('1988-04'),
+      other: 12345
+    });
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14').toISOString()).toBe('1988-04-14T00:00:00.000Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD strings to Date', () => {
+    test(interceptor, {testDate: '1988-04-14', other: '12345'}, {
+      testDate: new Date('1988-04-14'),
+      other: '12345'
+    });
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24').toLocaleString('de-de')).toBe('14.4.1988, 03:24:00');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm strings to Date', () => {
+    test(interceptor, {testDate: '1988-04-14T03:24', other: null}, {
+      testDate: new Date('1988-04-14T03:24'),
+      other: null
+    });
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD hh:mm strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14 03:24').toLocaleString('de-de')).toBe('14.4.1988, 03:24:00');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD hh:mm strings to Date', () => {
+    test(interceptor, '1988-04-14 03:24', new Date('1988-04-14 03:24'));
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11').toLocaleString('de-de')).toBe('14.4.1988, 03:24:11');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss strings to Date', () => {
+    test(interceptor, '1988-04-14T03:24:11', new Date('1988-04-14T03:24:11'));
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11.123').toLocaleString('de-de')).toBe('14.4.1988, 03:24:11');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss strings to Date', () => {
+    test(interceptor, '1988-04-14T03:24:11.123', new Date('1988-04-14T03:24:11.123'));
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss\'Z\' strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11.123Z').toISOString()).toBe('1988-04-14T03:24:11.123Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss\'Z\' strings to Date', () => {
+    test(
+      interceptor,
+      {
+        testDate: '1988-04-14T03:24:11.123',
+        inner: {testDate2: '1988-04-14T03:24:11.123'}
+      },
+      {testDate: new Date('1988-04-14T03:24:11.123'), inner: {testDate2: new Date('1988-04-14T03:24:11.123')}}
+    );
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss-hh:mm strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11.123-05:00').toISOString()).toBe('1988-04-14T08:24:11.123Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss-hh:mm strings to Date', () => {
+    test(interceptor, '1988-04-14T03:24:11.123-05:00', new Date('1988-04-14T03:24:11.123-05:00'));
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss+hh:mm strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11.123+05:30').toISOString()).toBe('1988-04-13T21:54:11.123Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss+hh:mm strings to Date', () => {
+    test(interceptor, {testDate: '1988-04-14T03:24:11.123+05:30'}, {testDate: new Date('1988-04-14T03:24:11.123+05:30')});
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss-hhmm strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11.123-0500').toISOString()).toBe('1988-04-14T08:24:11.123Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss-hhmm strings to Date', () => {
+    test(interceptor, {testDate: '1988-04-14T03:24:11.123-0500'}, {testDate: new Date('1988-04-14T08:24:11.123Z')});
   });
 
-  it('#convertToDate should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss+hhmm strings to Date', () => {
-    expect(interceptor['convertToDate']('1988-04-14T03:24:11.123+0530').toISOString()).toBe('1988-04-13T21:54:11.123Z');
+  it('#testAndConvert should convert (with default config) YYYY-MM-DD\'T\'hh:mm:ss.sss+hhmm strings to Date', () => {
+    test(interceptor, {testDate: '1988-04-14T03:24:11.123+0530'}, {testDate: new Date('1988-04-14T03:24:11.123+0530')});
   });
 
-  it('#convertToDate should not convert (with default config) non-ISO8601 Date-strings to Date', () => {
-    expect(interceptor['convertToDate']('December 17, 1995 03:24:00')).toBeNull();
+  it('#testAndConvert should not convert (with default config) non-ISO8601 Date-strings to Date', () => {
+    test(interceptor, {testDate: 'December 17, 1995 03:24:00'}, {testDate: 'December 17, 1995 03:24:00'});
   });
 
-  it('#convertToDate should not convert (with default config) other strings to Date', () => {
-    expect(interceptor['convertToDate']('abcdefg')).toBeNull();
-  });
-
-  it('#convertToDate should not convert (with default config) numbers to Date', () => {
-    expect(interceptor['convertToDate'](123)).toBeNull();
-  });
-
-  it('#convertToDate should not convert (with default config) objects to Date', () => {
-    expect(interceptor['convertToDate']({})).toBeNull();
-  });
-
-  it('#convertToDate should not convert (with default config) objects to Date', () => {
-    expect(interceptor['convertToDate']({})).toBeNull();
+  it('#testAndConvert should not convert (with default config) objects to Date', () => {
+    test(interceptor, {testDate: {}}, {testDate: {}});
   });
 
   it('performanceTest', () => {
@@ -104,10 +109,20 @@ describe('NgHttpDateInterceptor', () => {
     };
     const start = new Date().getTime();
     for (let i = 0; i < 1_000_000; i++) {
-      interceptor['testAndConvert'](Object.assign({}, testObj));
+      interceptor['testAndConvert'](convertToResponse(Object.assign({}, testObj)));
     }
     const time = new Date().getTime() - start;
     console.log('time = ' + time);
     expect(time).toBeGreaterThan(0);
   });
 });
+
+function test<T>(interceptor: NgHttpDateInterceptor, input: T, expected: object): void {
+  const response = convertToResponse<T>(input);
+  interceptor['testAndConvert'](response);
+  expect(JSON.stringify(response.body)).toBe(JSON.stringify(expected));
+}
+
+function convertToResponse<T>(something: T): HttpResponse<T> {
+  return new HttpResponse<T>({body: something});
+}
